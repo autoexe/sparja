351c698452b4f06323204c81c9b5cdeca1ef254a
Sergio <autoexec73@gmail.com>
2024-07-31 23:45:19 +1000

    init

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SingleproductPage = () => {
+  return <div>SingleproductPage</div>;
+};
+
+export default SingleproductPage;
---
+++ src/components/Notification.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notification = () => {
+  return (
+    <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
+      Бесплатная доставка для всех заказов свыше $50. Закажите еду сейчас!
+    </div>
+  );
+};
+
+export default Notification;
